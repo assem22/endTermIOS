ad501f225cf0b9540b965310a9052d19d78b59d1
assem22 <assem.mukhamadi@mail.ru>
2020-12-10 20:02:46 +0600

    Initial Commit

---
+++ EndTerm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EndTerm
+//
+//  Created by Assem Mukhamadi on 10.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
